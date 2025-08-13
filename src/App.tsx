e2392899d4963f4f2d6b3a84f579945c0e22b62f
honsiorovskyi <2880417+honsiorovskyi@users.noreply.github.com>
2025-08-14 00:59:46 +0200

    remove header

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,6 @@ import './App.css'
 function App() {
   return (
     <div class="app">
-      <header class="app-header">
-        <h1>HikeMap</h1>
-        <p>A simple map built with SolidJS and MapLibre GL JS</p>
-      </header>
       <main class="app-main">
         <MapComponent />
       </main>
